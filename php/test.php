e6165dee44916d0d81bdbabc60661349d6845d79
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2019-06-04 20:43:20 +0200

    Add php script

---
+++ php/test.php
@@ -0,0 +1,56 @@
+<?php
+
+// Example: $GPGGA,171544.000,4929.9587,N,00546.6857,E,1,6,1.59,348.7,M,47.6,M,,*56
+// Result: +49° 29.9587', +005° 46.6857'
+
+echo "Type 'GPGGA' NMEA sentence:\n";
+$nmeaSentence = readline("NMEA sentence: ");
+
+echo $nmeaSentence . "\n";
+
+echo gpgga2latlon($nmeaSentence) . "\n";
+
+function gpgga2latlon($sentence) {
+    $data = explode(",", $sentence);
+    $identifier = $data[0];
+    $time = $data[1];
+    $latitude = $data[2];
+    $latitudeDirection = $data[3];
+    $longitude = $data[4];
+    $longitudeDirection = $data[5];
+    $fixQuality = $data[6];
+    $NumberSatellites = $data[7];
+    $HorizontalDilutionPrecision = $data[8];
+    $altitudeMSL = $data[9];
+    $altitudeMSLUnity = $data[10];
+    $HeightGeoid = $data[11];
+    $HeightGeoidUnity = $data[12];
+    $TimeSinceDGPSUpdate = $data[13];
+    $DGPSReferenceStation = $data[13];
+    $Checksum = $data[14];
+
+    if($latitudeDirection == "N" or $longitudeDirection == "E") {
+        $latSign = $longSign = "+";
+    }
+
+    if($latitudeDirection == "S" or $longitudeDirection == "W") {
+        $latSign = $longSign = "-";
+    }
+
+    $dotPos = strpos($latitude, ".");
+    
+    $lat = substr($latitude, 0, $dotPos - 2);
+    $latTime = substr($latitude, $dotPos - 2);
+
+    $dotPos = strpos($longitude, ".");
+
+    $long = substr($longitude, 0, $dotPos - 2);
+    $longTime = substr($longitude, $dotPos - 2);
+
+    $result = $latSign . $lat . "° " . $latTime . "', " . $longSign . $long . "° " . $longTime . "'";
+
+    return $result;
+}
+
+
+?>
